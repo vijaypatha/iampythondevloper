3e299bb1b3805e0191219eea19a987579cf56e5d
Vijay Patha <vipatha@amazon.com>
2020-11-25 16:51:53 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -64,3 +64,38 @@ def prime_checker(number):
     print(f"{number} is Not a Prime")
 
 prime_checker(number = n)
+
+## Ceaser Ghost
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    # square brackets [] to access data in the list
+    cipher_text += new_letter
+  print(f"{cipher_text}")
+
+
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter2 in cipher_text:
+    position = alphabet.index(letter2)
+    new_position = position - shift_amount
+    new_letter = alphabet[new_position]
+    plain_text += new_letter
+  print(f"{plain_text}")
+    
+  
+
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text = text, shift_amount = shift)
+
